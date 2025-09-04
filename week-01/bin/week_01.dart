48fba89537f412323aabf4ec539799f4ccc45c97
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-09-04 14:26:29 +0700

    add `week-01` simple dart project

--- week-01/bin/week_01.dart
+++ week-01/bin/week_01.dart
@@ -1,5 +1,5 @@
-import 'package:week_01/week_01.dart' as week_01;
-
 void main(List<String> arguments) {
-  print('Hello world: ${week_01.calculate()}!');
+  for (int i = 18; i >= 9; i--) {
+    print('Nama saya adalah Fulan, sekarang berumur $i');
+  }
 }
