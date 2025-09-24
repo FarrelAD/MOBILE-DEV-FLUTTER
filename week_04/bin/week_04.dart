249cabbbfe2b4415e6fe6af1b35c1b4e78652fea
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-09-24 10:34:39 +0700

    refactor: put practicum-01 into separate program in lib directory

--- week_04/bin/week_04.dart
+++ week_04/bin/week_04.dart
@@ -1,19 +1,24 @@
+import 'dart:io';
+import 'package:week_04/practicum_1.dart';
+
 void main() {
-  // var list = [1, 2, 3];
-  // assert(list.length == 3);
-  // assert(list[1] == 2);
-  // print(list.length);
-  // print(list[1]);
+  print('Available practicum:');
+  for (int i = 1; i <= 5; i++) {
+    print('$i. Practicum');
+  }
 
-  // list[1] = 1;
-  // assert(list[1] == 1);
-  // print(list[1]);
+  stdout.write('Choose practicum: ');
+  String? inputStr = stdin.readLineSync();
 
-  List<String?> myList = [null, null, null, null, null];
-  myList[1] = 'Farrel AD';
-  myList[2] = '2341720081';
+  if (inputStr == null) {
+    print('No input received!');
+    return;
+  }
 
-  print(myList);
+  int inputInt = int.parse(inputStr);
 
-
+  switch (inputInt) {
+    case 1: practicum1(); break;
+    default: print('Practicum is not available!');
+  }
 }
