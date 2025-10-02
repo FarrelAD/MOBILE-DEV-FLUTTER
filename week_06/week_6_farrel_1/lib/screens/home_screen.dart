e4c060e6c36f253def39a747cb6441391da20b3b
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-10-02 15:03:32 +0700

    week-6: add practicum 1-(2-4)

--- week_06/week_6_farrel_1/lib/screens/home_screen.dart
+++ week_06/week_6_farrel_1/lib/screens/home_screen.dart
@@ -1,25 +1,102 @@
 import 'package:flutter/material.dart';
-import 'package:week_6_farrel_1/widgets/title_section_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  Widget _buildActionButton(IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [Icon(icon), Text(label)],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Flutter layout demo'),
         backgroundColor: Colors.white,
       ),
-      body: const Column(children: [TitleSectionWidget()]),
+      body: ListView(
+        children: [
+          Image.asset('assets/vespa.jpg'),
+
+          _buildTitleWidget(
+            title: 'Awesome Vespa 🤩!!',
+            location: 'Lowokwaru, Kota Malang',
+            totalRating: 34,
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButtonColumn(color, Icons.call, 'CALL'),
+              _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+              _buildButtonColumn(color, Icons.share, 'SHARE'),
+            ],
+          ),
+
+          Container(
+            padding: const EdgeInsets.all(32),
+            child: const Text(
+              'Sebuah Vespa modern hadir dengan desain elegan yang memadukan gaya klasik ikonik dengan sentuhan teknologi masa kini. Garis bodi yang halus dan lampu LED futuristik memberikan kesan premium, sementara mesin bertenaga serta fitur-fitur canggih seperti panel digital membuat pengalaman berkendara semakin nyaman. Cocok untuk gaya hidup urban, Vespa modern bukan hanya sekadar kendaraan, tetapi juga simbol tren dan gaya hidup yang dinamis.',
+              softWrap: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTitleWidget({
+    required String title,
+    required String location,
+    int totalRating = 0,
+  }) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    title,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(location, style: const TextStyle(color: Colors.red)),
+              ],
+            ),
+          ),
+
+          /* soal 3*/
+          const Icon(Icons.star, color: Colors.red),
+          Text('$totalRating'),
+        ],
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
