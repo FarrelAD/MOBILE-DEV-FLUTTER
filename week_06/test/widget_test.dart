461c65704e0107bb5a91afdbf84777f86d8dce43
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-10-05 23:14:54 +0700

    week-6(refactor): move flutter project from `week_6_farrel_1` to `week_6`

--- week_06/test/widget_test.dart
+++ week_06/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:week_6_farrel_1/main.dart';
+import 'package:week_6/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
